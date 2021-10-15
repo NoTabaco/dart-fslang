9b446fbd3a619b2966ec3e7bbd559b56d3b17155
SevenStars <32127313+NoTabaco@users.noreply.github.com>
2021-10-15 17:58:04 +0900

    Updated Data Organization

--- Program.fs
+++ Program.fs
@@ -2,25 +2,12 @@
 
 open FSharp.Data
 
-type CompanyData() =
-    class
-        let mutable name = ""
-        let mutable cir = ""
-        let mutable link = ""
-
-        member x.Insert(n, c, l) =
-            name <- n
-            cir <- c
-            link <- l
-    end
-
-
 [<EntryPoint>]
 let main argv =
     let url =
         "https://dart.fss.or.kr/dsac001/mainY.do?selectDate=2021.10.14"
 
-    let prefixURL = "https://dart.fss.or.kr/"
+    let prefixURL = "https://dart.fss.or.kr"
 
     let results = HtmlDocument.Load(url)
 
@@ -51,15 +38,33 @@ let main argv =
                         x.TryGetAttribute("href")
                         |> Option.map (fun a -> x.InnerText(), a.Value())))
 
-    let mutable companyData = [||]
+    let mutable companyDataArr = [||]
     let mutable arrDatas = datas |> Seq.toArray
     let datasLength = datas |> Seq.length
 
     for i in 0 .. datasLength - 1 do
-        let mutable (name, href) = arrDatas.[i]
+        let name, _ = arrDatas.[i]
+        let trimName = name.Trim()
 
-        if name = "주주총회소집공고" then
-            companyData <- [| name, href |] |> Array.append companyData
-            printfn "%A" companyData
+        if
+            trimName.Equals("주주총회소집공고")
+            || trimName.Equals("[기재정정]주주총회소집공고")
+        then
+            // Corporate opening information (href)
+            let companyName, _ = arrDatas.[i - 1]
+            let trimCompanyName = companyName.Trim()
+            // Cir
+            let cir, cirHref = arrDatas.[i]
+            let trimCirName = cir.Trim()
+
+            let companyData =
+                [ trimCompanyName, trimCirName, prefixURL + cirHref ]
+
+            companyDataArr <-
+                companyData
+                |> List.toArray
+                |> Array.append companyDataArr
+
+            printfn "%A" companyDataArr
 
     0
